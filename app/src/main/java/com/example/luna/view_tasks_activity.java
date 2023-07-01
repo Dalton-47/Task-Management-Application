46a5f453160ca873f81a8d0940afecfcf54beb26
DALTON LEYIAN <daltonleyian@gmail.com>
2023-07-01 14:59:17 +0300

    Added Tasks View Page

--- app/src/main/java/com/example/luna/view_tasks_activity.java
+++ app/src/main/java/com/example/luna/view_tasks_activity.java
@@ -1,14 +1,27 @@
 package com.example.luna;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class view_tasks_activity extends AppCompatActivity {
+ task_adapter taskAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_tasks);
+
+        RecyclerView taskRecyclerView=(RecyclerView)  this.findViewById(R.id.recyclerViewTasks);
+
+        taskAdapter = new task_adapter();
+        taskRecyclerView.setAdapter(taskAdapter);
+
+        LinearLayoutManager linearLayout=new LinearLayoutManager(this);
+        taskRecyclerView.setLayoutManager(linearLayout);
+
+
     }
 }
