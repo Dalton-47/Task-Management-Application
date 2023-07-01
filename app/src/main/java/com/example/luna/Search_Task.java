b13ea530f4aa286c45d2d749a0116155ee246a8e
DALTON LEYIAN <daltonleyian@gmail.com>
2023-07-01 13:45:46 +0300

    First Commit

---
+++ app/src/main/java/com/example/luna/Search_Task.java
@@ -0,0 +1,14 @@
+package com.example.luna;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Search_Task extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.search_task_activity);
+    }
+}
